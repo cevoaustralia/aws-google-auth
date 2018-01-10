55d91fdfbeb047c8afcee31e013b74baa97d92ef
Mark Ide <mide@circle.com>
2018-01-10 13:25:45 -0500

    Python version differences.

--- aws_google_auth/tests/test_util.py
+++ aws_google_auth/tests/test_util.py
@@ -16,14 +16,14 @@ class TestUtilMethods(unittest.TestCase):
     def test_unicode_to_string_if_needed_python_3(self):
         if sys.version_info >= (3, 0):
             value_string = "Test String!"
-            self.assertIsInstance(value_string, str)
+            self.assertIn("str", str(value_string.__class__))
             self.assertEqual(util.Util.unicode_to_string_if_needed(value_string), value_string)
 
     def test_unicode_to_string_if_needed_python_2(self):
         if sys.version_info < (3, 0):
             value_string = "Test String!"
             value_unicode = value_string.decode('utf-8')
-            self.assertIsInstance(value_string, str)
-            self.assertIsInstance(value_unicode, unicode)
+            self.assertIn("str", str(value_string.__class__))
+            self.assertIn("unicode", str(value_unicode.__class__))
             self.assertEqual(util.Util.unicode_to_string_if_needed(value_unicode), value_string)
             self.assertEqual(util.Util.unicode_to_string_if_needed(value_string), value_string)
